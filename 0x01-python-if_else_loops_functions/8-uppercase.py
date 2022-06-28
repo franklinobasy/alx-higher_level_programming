3ddbb5cca784b966212b7652640ea3607a4a9003
Franklin Obasi <franklinobasy@gmail.com>
2022-06-28 13:09:15 +0100

    8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    special_chars = " ,"
     for c in str:
         ascii = ord(c)
-        ascii = ascii - 32
+        if c not in special_chars:
+            if ascii >= 97:
+                ascii = ascii - 32
         print("{0:c}".format(ascii), end="")
     print()
