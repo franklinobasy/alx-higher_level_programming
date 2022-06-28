e4481e7674e6f1e4226adf684c9f4b5253378802
Franklin Obasi <franklinobasy@gmail.com>
2022-06-28 14:46:53 +0100

    15. Remove at position

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * insert_node -  function in C that inserts a number into a sorted singly linked list.
+ * 
+ * @head: pointer to head pointer
+ * @number: number to insert
+ * @return: listint_t* 
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *prev, *later, *new, *current
+
+    current = *head
+    *new = NULL
+
+    while (current != NULL)
+    {
+        
+    }
+
+
+
+}
