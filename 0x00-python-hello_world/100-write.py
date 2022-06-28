0321d9f0bb63be8f48828bb105b066f49af1a1f6
Franklin Obasi <franklinobasy@gmail.com>
2022-06-28 08:39:50 +0100

    11. Hello, write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 import sys
-str = "and that piece of art is useful - Dora Kopar, 2015-10-19\n"
-sys.stderr.write(str)
+
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
 sys.exit(1)
