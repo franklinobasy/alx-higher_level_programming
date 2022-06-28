38d707abadab0ff64277336f899b4ed0f5041684
Franklin Obasi <franklinobasy@gmail.com>
2022-06-28 10:27:34 +0100

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,8 +2,5 @@
 
 i = 0
 while i <= 99:
-
-    print(f"0{i}" if i < 10 else f"{i}", \
-        end = ", " if i < 99 else "\n")
-
+    print(f"0{i}" if i < 10 else f"{i}", end=", " if i < 99 else "\n")
     i += 1
