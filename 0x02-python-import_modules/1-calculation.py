fed32879b5ccb6600f9ebd1fd13ecbe1975c6a17
Franklin Obasi <franklinobasy@gmail.com>
2022-06-30 12:18:16 +0100

    1. My first toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
 if __name__ == '__main__':
     a = 10
     b = 5
