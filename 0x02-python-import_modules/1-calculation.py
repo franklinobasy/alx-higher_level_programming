6abc4e0112b0385141f2fda89c00eaee0de981e7
Franklin Obasi <franklinobasy@gmail.com>
2022-06-30 12:35:33 +0100

    1. My first toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,10 +4,6 @@ from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     a = 10
     b = 5
-    # print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
-    # print("{0:d} + {1:d} = {2:d}".format(a, b, sub(a, b)))
-    # print("{0:d} + {1:d} = {2:d}".format(a, b, mul(a, b)))
-    # print("{0:d} + {1:d} = {2:d}".format(a, b, div(a, b)))
     print(a, " + ", b, " = ", add(a, b))
     print(a, " - ", b, " = ", sub(a, b))
     print(a, " * ", b, " = ", mul(a, b))
