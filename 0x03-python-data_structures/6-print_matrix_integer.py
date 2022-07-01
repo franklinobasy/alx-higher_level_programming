5dcc97d4a958105d9daa9a80f0ea7766fbbbb212
Franklin Obasi <franklinobasy@gmail.com>
2022-07-01 12:46:31 +0100

    6. Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    n_row = len(matrix)
+    n_col = len(matrix[0])
+    if n_row <= 1 or n_col <= 1:
+        print()
+    else:
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if col == 2:
+                    end = "\n"
+                else:
+                    end = " "
+                print("{:d}".format(matrix[row][col]), end=end)
