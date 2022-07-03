fe20117720785304b56af38e86630d6352f6fa0e
Franklin Obasi <franklinobasy@gmail.com>
2022-07-04 00:27:06 +0100

    14. CPython #0: Python lists

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,81 +1,31 @@
 #include "lists.h"
 
 /**
- * is_palindrome -  checks if a singly linked list is a palindrome.
- * 
- * @head: pointer to linked list head
- * 
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- * 
+ * palindrom -  recursive palind or not
+ * @head: head list
+ * Return: 0 if it is not a palindrome
+ * 1 if it is a palindrome
  */
-
 int is_palindrome(listint_t **head)
 {
-		int *values;
-		listint_t *current, *nodes;
-		unsigned int n, i, value, j;
-
-		if (head == NULL)
-			return (0);
-
-		n = n_listint(*head);
-		values = malloc(n * sizeof(int));
-		nodes = malloc(n * sizeof(listint_t));
-
-		current = *head;
-		if (current == NULL)
+		if (head == NULL || *head == NULL)
 				return (1);
-
-		i = 0;
-		while (current != NULL)
-		{
-				value = current->n;
-				values[i] = value;
-				nodes[i] = *current;
-				current = current->next;
-				i++;
-		}
-
-		i--;
-		n = 1;
-		j = 0;
-		while (i >= n)
-		{
-				current = &nodes[i];
-				if (current->n != values[j])
-				{
-					free(values);
-					free(nodes);
-					return (0);
-				}
-				i--;
-				j++;
-		}
-
-		free(values);
-		free(nodes);
-
-		return (1);
+		return (aux_palind(head, *head));
 }
 
 /**
- * n_listint - counts elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * aux_palind - funct to know if is palindrome
+ * @head: head list
+ * @end: end list
  */
-
-size_t n_listint(const listint_t *h)
+int aux_palind(listint_t **head, listint_t *end)
 {
-		const listint_t *current;
-		unsigned int n; /* number of nodes */
-
-		current = h;
-		n = 0;
-		while (current != NULL)
+		if (end == NULL)
+				return (1);
+		if (aux_palind(head, end->next) && (*head)->n == end->n)
 		{
-				current = current->next;
-				n++;
+				*head = (*head)->next;
+				return (1);
 		}
-
-		return (n);
+		return (0);
 }
