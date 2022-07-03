dd51d0c1cb4ad2f4ebcc7bed45620c81972b9e67
Franklin Obasi <franklinobasy@gmail.com>
2022-07-03 13:02:36 +0100

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_string = ""
     for char in my_string:
         if char not in "cC":
-            new_string+=char
+            new_string += char
 
     return new_string
