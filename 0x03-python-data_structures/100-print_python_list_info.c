988c7c7e9d4227a73fec763ce3d5f46793967e8d
Franklin Obasi <franklinobasy@gmail.com>
2022-07-04 19:34:50 +0100

    14. CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -26,8 +26,7 @@ void print_python_list_info(PyObject *p)
 	while (i < length)
 	{
 		element = PyList_GetItem(p, i);
-		printf("Element %d: ", i);
-		printf("%s\n", Py_TYPE(element)->tp_name);
+		printf("Element %d: %s\n", i, Py_TYPE(element)->tp_name);
 		i++;
 	}
 }
