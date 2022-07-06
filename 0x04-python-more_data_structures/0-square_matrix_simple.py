9deb80782a22ff03ef3df3a0efb14ff76635e58d
Franklin Obasi <franklinobasy@gmail.com>
2022-07-06 13:11:29 +0100

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sq_list = lambda x: [i**2 for i in x]
     new_list = []
     for row in matrix:
         new_row = sq_list(row)
         new_list.append(new_row)
     return new_list
+
+
+def sq_list(x):
+    return [i**2 for i in x]
