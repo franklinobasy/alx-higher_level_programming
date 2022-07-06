2aec951da121c61015e21a53991c73b34bf3b44f
Franklin Obasi <franklinobasy@gmail.com>
2022-07-06 15:54:54 +0100

    13. Weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
+        return 0
+    else:
         w_average = 0
         for score, weight in my_list:
             weight_average += (score * w_average)
         return w_average
-    return 0
