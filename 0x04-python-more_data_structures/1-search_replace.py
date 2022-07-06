934bc1c9affd9fce06c918fc75f77e55a13927f5
Franklin Obasi <franklinobasy@gmail.com>
2022-07-06 13:26:37 +0100

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,5 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     if search in my_list:
         s_index = my_list.index(search)
-        new_list[s_index] = replace    
+        new_list[s_index] = replace
     return new_list
-    
