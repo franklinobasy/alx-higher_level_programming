d7791f5a20ff7bf3f2440716a01a67a721622bf9
Franklin Obasi <franklinobasy@gmail.com>
2022-07-18 14:36:47 +0100

    7. Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        r = value / 1
+        print("{:d}".format(value))
+        return True
+    except TypeError as e:
+        sys.stderr.write("Exception: " + e.args[0])
+        return False
+        
+        
