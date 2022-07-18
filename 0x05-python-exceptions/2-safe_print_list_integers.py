5fe2b1a133183890fda09619f3ddfb6517d61685
Franklin Obasi <franklinobasy@gmail.com>
2022-07-18 12:50:44 +0100

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     real = 0
-    
+
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             real += 1
         except (ValueError, TypeError):
             pass
-    
+
     print()
     return real
