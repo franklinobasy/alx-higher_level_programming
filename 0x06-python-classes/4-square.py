b67104f20fa9fd408beeafd2aa6fa8761519beab
Franklin Obasi <franklinobasy@gmail.com>
2022-07-19 14:50:16 +0100

    4. Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -25,4 +25,8 @@ class Square():
     """size setter"""
     @size.setter
     def size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
