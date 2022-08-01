87312358d57c8bbffcca67040ebe9c4103357ad0
Franklin Obasi <franklinobasy@gmail.com>
2022-08-01 16:34:59 +0100

    2. Exact same object

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-file: 4-inherits_from.py
-Class:
--> inherits_from
+"""4-inherits_from.py
 """
 
 
 def inherits_from(obj, a_class):
-    """ Check for directly or indirectly inheritance """
+    """ Check for direct or indirect inheritance """
     if (type(obj) != a_class):
         return isinstance(obj, a_class)
     return False
