0b5661e784e06fc00ed362fa4801c86d1377e86e
Franklin Obasi <franklinobasy@gmail.com>
2022-08-01 16:33:21 +0100

    2. Exact same object

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""4-inherits_from.py
+"""
+file: 4-inherits_from.py
+Class:
+-> inherits_from
 """
 
 
 def inherits_from(obj, a_class):
-    """
-        function that returns True if the object is an instance of a class that inherited
-        (directly or indirectly) from the specified class ; otherwise False.
-    """
+    """ Check for directly or indirectly inheritance """
     if (type(obj) != a_class):
         return isinstance(obj, a_class)
     return False
