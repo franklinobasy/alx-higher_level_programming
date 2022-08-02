0610d1316d00e1989a43e7e2de7e42c58b9c6a92
Franklin Obasi <franklinobasy@gmail.com>
2022-08-02 23:11:25 +0100

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""
- save items to a file.
-"""
+'''7-add_item.py
+    a script that adds all arguments to a Python list,
+    and then save them to a file
+'''
 
+from os import path
 import sys
-import os.path
 
-args = sys.argv[1:]
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    argv = sys.argv[1:]
 
-item = []
-if os.path.exists("./add_item.json"):
-    item = load_from_json_file("add_item.json")
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-save_to_json_file(item + args, "add_item.json")
+    file_path = "add_item.json"
+    if path.exists(file_path):
+        list_obj = load_from_json_file(file_path)
+    else:
+        list_obj = list()
+    
+    save_to_json_file(list_obj + argv, file_path)
+    print(end="")
