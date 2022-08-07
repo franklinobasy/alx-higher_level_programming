7ccc6e7a2b52749e820af224c9ceeb6c4d5475f6
Franklin Obasi <franklinobasy@gmail.com>
2022-08-07 22:11:51 +0100

    20. JSON ok, but CSV?

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,6 +58,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
+        ''' returns a list of instances from file'''
         try:
             with open(f"{cls.__name__}.json", 'r') as file_object:
                 json_string = file_object.read()
@@ -71,4 +72,53 @@ class Base:
             return obj_list
 
         except FileNotFoundError:
-            return list
+            return list()
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        " serializes to CSV"
+        to_write = ""
+        for obj in list_objs:
+            obj_dict = obj.to_dictionary()
+            if "size" in obj_dict.keys():
+                attrs = ["id", "size", "x", "y"]
+            else:
+                attrs = ["id", "width", "height", "x", "y"]
+            for attr in attrs:
+                if attr == "y":
+                    to_write += f"{obj_dict[attr]}"
+                else:
+                    to_write += f"{obj_dict[attr]},"
+            to_write += "\n"
+
+        file_name = f"{cls.__name__}.csv"
+        with open(file_name, "w") as file_obj:
+            file_obj.write(to_write)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        ''' deserializes from CSV'''
+        try:
+            with open(f"{cls.__name__}.csv", 'r') as file_object:
+                csv_strings = file_object.readlines()
+
+            if cls.__name__ == "Square":
+                attrs = ["id", "size", "x", "y"]
+            else:
+                attrs = ["id", "width", "height", "x", "y"]
+
+            objs_list = []
+            for csv_string in csv_strings:
+                values = csv_string.split(',')
+                values = [int(val) for val in values]
+                obj_dict = {}
+                for index in range(len(values)):
+                    obj_dict[attrs[index]] = values[index]
+
+                obj = cls.create(**obj_dict)
+                objs_list.append(obj)
+
+            return objs_list
+
+        except FileNotFoundError:
+            return list()
