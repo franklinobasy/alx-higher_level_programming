080d4c80fa5aa3275136c2f8caa7f23c15f711e9
Franklin Obasi <franklinobasy@gmail.com>
2022-09-06 23:15:21 +0100

    10. List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,6 @@
 -- a script that lists all records of a table
 -- lists all records of the table second_table of the database hbtn_0c_0
-SELECT score, name
-    FROM second_table
-        ORDER BY score DESC;
+SELECT
+    score, name
+FROM
+    second_table ORDER BY score DESC;
