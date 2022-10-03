1d797426ad9a28b3e1b7ae746c734da771b77fc0
Franklin Obasi <franklinobasy@gmail.com>
2022-10-03 21:53:43 +0100

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (const line of arr) {
   console.log(line);
 }
