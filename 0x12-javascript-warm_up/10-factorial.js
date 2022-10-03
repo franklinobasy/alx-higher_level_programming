cf02e8e45f57f92ba1a4bedbfa8bcea99ccbf1b0
Franklin Obasi <franklinobasy@gmail.com>
2022-10-03 22:33:02 +0100

    10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+}
 
-const factorial = (number) => {
-  if (isNaN(number)) return 1;
-
-  if (number === 1) return 1;
-
-  return number * factorial(number - 1);
-};
-
-console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(Number(process.argv[2])));
