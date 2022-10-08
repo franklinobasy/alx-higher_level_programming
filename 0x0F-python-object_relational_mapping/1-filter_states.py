eea1ea4e5241446b68130e34584a662a05a11931
Franklin Obasi <franklinobasy@gmail.com>
2022-10-08 17:21:03 +0100

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,43 +1,28 @@
 #!/usr/bin/python3
 '''
-A script that lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa
+lists all states with a name starting with N
 '''
 
-import sys
 import MySQLdb
+import sys
 
-
-def list_states(username, password, database_name):
-    '''This functions prints all states in the database'''
-
-    connection = MySQLdb.connect(
-        host="localhost",
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         port=3306,
-        user=username,
-        passwd=password,
-        db=database_name,
-        charset="utf8",
-    )
+        host='localhost')
 
-    cursor = connection.cursor()
+    cursor = db.cursor()
+    cursor.execute("SELECT * \
+                    FROM states \
+                    WHERE CONVERT(`name` USING Latin1) \
+                    COLLATE Latin1_General_CS \
+                    LIKE 'N%';")
 
-    query = '''
-        SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;
-    '''
-
-    cursor.execute(query)
-
-    results = cursor.fetchall()
-
-    for row in results:
-        print(row)
-
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
     cursor.close()
-    connection.close()
-
-
-if __name__ == "__main__":
-    argv = sys.argv[1:]
-    username, password, db_name = argv
-    list_states(username, password, db_name)
+    db.close()
