ce517c269deffb817110c5de99f48d36bf638f5e
Franklin Obasi <franklinobasy@gmail.com>
2022-10-08 18:50:17 +0100

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 '''
-a script that takes in an argument and displays
-all values in the states table of hbtn_0e_0_usa
-where name matches the argument.
+A script that lists all cities from the database hbtn_0e_4_usa
 '''
 
 import sys
