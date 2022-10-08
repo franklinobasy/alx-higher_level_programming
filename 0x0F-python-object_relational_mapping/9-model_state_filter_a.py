ed82e0ea67fac25ee8d6e07d061a74d08df6f0ef
Franklin Obasi <franklinobasy@gmail.com>
2022-10-08 23:52:17 +0100

    9. Contains

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+'''
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(
+        url.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    data = session.query(State).order_by(State.id)\
+                               .filter(State.name.like("%a%")).all()
+    for row in data:
+        print("{}: {}".format(row.id, row.name))
+    session.close()
