53623c6643fa73c84a983f4bb32822346bb62ab9
Franklin Obasi <franklinobasy@gmail.com>
2022-10-09 09:19:14 +0100

    15. City relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that deletes all State objects with a name
+containing the letter a from the database hbtn_0e_6_usa
+"""
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3])
+        )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='California')
+    new_city = City(name='San Francisco', state=new_state)
+
+    session.add(new_city)
+    session.commit()
+    session.close()
