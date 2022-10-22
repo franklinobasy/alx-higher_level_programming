aee2ae027a102434f71e2cd33e57e52e2cdd2989
Franklin Obasi <franklinobasy@gmail.com>
2022-10-22 03:07:41 +0100

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     req = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(req) as resp:
-         print(resp.read().decode("utf-8"))
+        print(resp.read().decode("utf-8"))
