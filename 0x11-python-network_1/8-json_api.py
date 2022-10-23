24bebcd896e99f4f212b3d406128cc0baf4f087c
Franklin Obasi <franklinobasy@gmail.com>
2022-10-23 20:56:36 +0100

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+"""script that takes in a letter and
+sends a POST request to
+http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
-Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter
-"""
-
+import requests
+import sys
 
 if __name__ == '__main__':
-    import requests
-    from sys import argv
-
-    q = argv[1] if len(argv) == 2 else ""
     url = 'http://0.0.0.0:5000/search_user'
-    r = requests.post(url, data={'q': q})
-    r_dict = r.json()
-    try:
-        id, name = r_dict.get('id'), r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
-            print("No result")
+    if len(sys.argv) != 2:
+        print("No result")
+    else:
+        letter = sys.argv[1]
+        if letter:
+            data = {'q': sys.argv[1]}
         else:
-            print(f"[{r_dict.get('id')}] {r_dict.get('name')}")
-    except Exception as e:
-        print("Not a valid JSON")
+            data = {'q': ''}
+        response = requests.post(url, data)
+        try:
+            json_response = response.json()
+            if not json_response.get('id'):
+                print("No result")
+            else:
+                print(f"[{json_response.get('id')}] {json_response.get('name')}")
+        except:
+            print("Not a valid JSON")
