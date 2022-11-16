700b0ed2e1857268dfca5f1236d73dc83a2cf886
Franklin Obasi <franklinobasy@gmail.com>
2022-11-16 14:21:36 +0100

    4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv.slice(2);
+const uri = args[0];
+
+request(uri, (err, resp, body) => {
+  if (err) {
+    console.err(err);
+  } else {
+    const results = JSON.parse(body).results;
+    let n = 0;
+
+    results.forEach(result => {
+      let characters = result.characters;
+
+      characters.forEach(character => {
+        if (character.endsWith('18/')) {
+          n++;
+        }
+      });
+    });
+    console.log(n);
+  }
+});
