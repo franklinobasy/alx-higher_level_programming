5f169e6b2da9fad7c0eef6dd3404c09d621eca81
Franklin Obasi <franklinobasy@gmail.com>
2022-11-16 12:06:53 +0100

    2. Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv.slice(2);
+
+request
+  .get(args[0])
+  .on('response', (response) => {
+    console.log(`code: ${response.statusCode}`);
+  })
+  .on('error', (err) => {
+    console.error(err);
+  });
