360e030715c61c618932306fc1b608772b66785f
Franklin Obasi <franklinobasy@gmail.com>
2022-11-16 08:50:24 +0100

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-const fs = require("fs");
+const fs = require('fs');
 
 fs.readFile(args[0], 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        console.log(data);
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
