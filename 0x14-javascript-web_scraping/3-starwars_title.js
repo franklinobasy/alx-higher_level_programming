e0413d8067d76894e5895db5fcaf7db77f2f0caa
Franklin Obasi <franklinobasy@gmail.com>
2022-11-16 13:48:14 +0100

    3. Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const args = process.argv.slice(2);
-const uri = `https://swapi-api.hbtn.io/api/films/${args[0]}`
+const uri = `https://swapi-api.hbtn.io/api/films/${args[0]}`;
 
 request(uri, (err, response, body) => {
   if (err) {
