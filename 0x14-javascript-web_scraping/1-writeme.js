f4a6c2b3a63422f50aebae6e329e8c53aab8177b
Franklin Obasi <franklinobasy@gmail.com>
2022-11-16 09:10:26 +0100

    1. Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const fs = require("fs");
+
+const options = {
+  encoding: 'utf8',
+  flag: 'w'
+};
+
+fs.writeFile(args[0], args[1], options, (err,) => {
+  if (err) {
+    console.error(err);
+  }
+});
